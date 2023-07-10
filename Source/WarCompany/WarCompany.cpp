85e4a5d2e6d3943bdc1fc686c48fc79332ce8372
Max <64340192+Max-Light@users.noreply.github.com>
2023-07-10 14:46:50 -0700

    Initial commit

---
+++ Source/WarCompany/WarCompany.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WarCompany.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarCompany, "WarCompany" );
---
+++ Source/WarCompany/WarCompanyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarCompanyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARCOMPANY_API AWarCompanyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
